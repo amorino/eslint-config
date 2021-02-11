c0d54ab9a7d512d46607a2265d2bfe0ed310e7d1
Erick Bazán <erick.bazan@me.com>
2021-02-11 10:54:21 -0600

    feat: don’t try types

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -74,9 +74,6 @@ module.exports = {
   },
   settings: {
     "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true,
-      },
       node: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
